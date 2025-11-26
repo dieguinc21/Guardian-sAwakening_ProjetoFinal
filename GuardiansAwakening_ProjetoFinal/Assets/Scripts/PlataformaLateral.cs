dbb153276a6d4995c1a12e8a9c412c6b6366164b
diegoooo <diego.cabral@escolar.ifrn.edu.br>
2025-11-26 20:30:17 -0300

    att

--- GuardiansAwakening_ProjetoFinal/Assets/Scripts/PlataformaLateral.cs
+++ GuardiansAwakening_ProjetoFinal/Assets/Scripts/PlataformaLateral.cs
@@ -18,13 +18,30 @@ public class PlataformaLateral : MonoBehaviour
         // Move a plataforma até o alvo
         transform.position = Vector3.MoveTowards(transform.position, alvo, velocidade * Time.deltaTime);
 
-        // Quando chega no ponto, troca o alvo
+        // Troca o alvo quando chega
         if (Vector3.Distance(transform.position, alvo) < 0.05f)
         {
             if (alvo == pontoDireita.position)
                 alvo = pontoEsquerda.position;
             else
                 alvo = pontoDireita.position;
+        }
+    }
+
+    // --- LÓGICA QUE GRUDA O PLAYER NA PLATAFORMA ---
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            collision.transform.SetParent(transform); // player vira filho da plataforma
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            collision.transform.SetParent(null); // volta ao normal
         }
     }
 }
